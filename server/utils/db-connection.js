f14db48f3f791b7593e46ec3e85d5f89234bdbd0
ramyasunarkani <ramyakrishnasunarkani@gmail.com>
2025-09-05 16:00:32 +0530

    first

--- server/utils/db-connection.js
+++ server/utils/db-connection.js
@@ -8,7 +8,8 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: "postgres",
-    logging:false
+    logging: false,
+    dialectOptions: process.env.DB_SSL === "true" ? { ssl: { rejectUnauthorized: false } } : {}
   }
 );
 
